4bdb7b2ffbea39733e4504951a8d7c5010905dd2
ohhyerim <tmvkfmxk00@naver.com>
2022-09-18 16:17:53 +0900

    반응형

--- asset/js/common.js
+++ asset/js/common.js
@@ -5,24 +5,24 @@ $(function(){
         /**
          * intro
          */
-            const introAni2 = gsap.timeline({
-            paused:true
-            })
-            introAni2.addLabel('a')
-            .from('.sc-visual .sc-title .tit-msg',{opacity:0, yPercent:103, stagger:0.2,duration:0.6},'a')
-            .from('.sc-visual .thumb-area',{top:"-70%",duration:1},'a') // top컨트롤
-            .from('.header',{opacity:0,duration:1.2},'a')
+            // const introAni2 = gsap.timeline({
+            // paused:true
+            // })
+            // introAni2.addLabel('a')
+            // .from('.sc-visual .sc-title .tit-msg',{opacity:0, yPercent:103, stagger:0.2,duration:0.6},'a')
+            // .from('.sc-visual .thumb-area',{top:"-70%",duration:1},'a') // top컨트롤
+            // .from('.header',{opacity:0,duration:1.2},'a')
         
-            gsap.set('.loading .loading-thumb',{xPercent:1000})
-            const introAni = gsap.timeline({
-                onComplete:function(){
-                    introAni2.play();
-                }
-            })
-            introAni.addLabel('a')
-            .to('.loading .loading-thumb',{xPercent:-10,duration:3},'a')
-            .from('.loading .loading-txt',{opacity:0,duration:1,delay:3},'a')
-            .to('.loading',{opacity:0,delay:5,duration:1,display:'none'},'a')
+            // gsap.set('.loading .loading-thumb',{xPercent:1000})
+            // const introAni = gsap.timeline({
+            //     onComplete:function(){
+            //         introAni2.play();
+            //     }
+            // })
+            // introAni.addLabel('a')
+            // .to('.loading .loading-thumb',{xPercent:-10,duration:3},'a')
+            // .from('.loading .loading-txt',{opacity:0,duration:1,delay:3},'a')
+            // .to('.loading',{opacity:0,delay:5,duration:1,display:'none'},'a')
         
         },
           // laptop
@@ -30,23 +30,23 @@ $(function(){
         /**
          * intro
          */
-            const introAni2 = gsap.timeline({
-                paused:true
-            })
-            introAni2.addLabel('a')
-            .from('.sc-visual .thumb-area',{transform:"translateY(-120%)",duration:1},'a') // top컨트롤
-            .from('.header',{opacity:0,duration:1.2},'a')
+            // const introAni2 = gsap.timeline({
+            //     paused:true
+            // })
+            // introAni2.addLabel('a')
+            // .from('.sc-visual .thumb-area',{transform:"translateY(-120%)",duration:1},'a') // top컨트롤
+            // .from('.header',{opacity:0,duration:1.2},'a')
 
-            gsap.set('.loading .loading-thumb',{xPercent:1000})
-            const introAni = gsap.timeline({
-                onComplete:function(){
-                    introAni2.play();
-                }
-            })
-            introAni.addLabel('a')
-            .to('.loading .loading-thumb',{xPercent:-10,duration:3},'a')
-            .from('.loading .loading-txt',{opacity:0,duration:1,delay:3},'a')
-            .to('.loading',{opacity:0,delay:5,duration:1,display:'none'},'a') 
+            // gsap.set('.loading .loading-thumb',{xPercent:1000})
+            // const introAni = gsap.timeline({
+            //     onComplete:function(){
+            //         introAni2.play();
+            //     }
+            // })
+            // introAni.addLabel('a')
+            // .to('.loading .loading-thumb',{xPercent:-10,duration:3},'a')
+            // .from('.loading .loading-txt',{opacity:0,duration:1,delay:3},'a')
+            // .to('.loading',{opacity:0,delay:5,duration:1,display:'none'},'a') 
           
         },
         // all
